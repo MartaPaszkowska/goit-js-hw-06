be43a2097e6685f692dcfbad5b2a96401f305559
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2023-11-03 12:11:31 +0100

    ex 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
 	"Herbs",
 	"Condiments",
 ];
+const list = document.querySelector("ul#ingredients");
+
+for (const ingredient of ingredients) {
+	const ingredientsList = document.createElement("li");
+	ingredientsList.textContent = ingredient;
+	ingredientsList.classList.add("item");
+	list.append(ingredientsList);
+}
